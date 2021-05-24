0af53d248c49655d9fe18f41b79cebf7fcdf98b5
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-05-24 15:31:17 -0700

    adding server.js:

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+
+//adding PORT and app Express//
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//adding Express.js middleware//
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// testing connection to PORT//
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+//function that will start the server( KEEP this at the bottom)//
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
